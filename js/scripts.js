e3b6c3c367c8ffa003f855bf0a036ded1fb46c4f
jkipker <jkipker@gmail.com>
2014-11-23 15:48:06 -0800

    responsive landing page layout complete

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 
 $(function() {
 	
+		//dynamic events to call on browser resize
     	$(window).resize(function() {
     		resizeElements();
 	    });
@@ -11,6 +12,28 @@ $(function() {
 	    	var contentWidth = contentWidth = $('.container').width()-330;
 			$('.content-pillar').css( "width", contentWidth );
 	    }
+
+	    /**BUTTON & HOVER FUNTIONS**/
+
+	    //Landing Page Column hover
+	    $(".content-pillar .column").hover(
+		  function() {
+		    $(".overlay", this).css( "bottom", "0px" );
+		  }, function() {
+		    $(".overlay", this).css( "bottom", "-140px" );
+		  }
+		);
+
+	    //legal hover over
+	    $(".nav-pillar .legal").hover(
+		  function() {
+		    $(this).css( "bottom", "0px" );
+		  }, function() {
+		    $(this).css( "bottom", "-400px" );
+		  }
+		);
+
+
 
 	    resizeElements();
 });
