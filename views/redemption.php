66363612a6f3f2f460a0d6dfecacf8d05c8ced8b
jkipker <jkipker@gmail.com>
2014-12-02 17:08:27 -0800

    redemption page responsive to 1345

--- views/redemption.php
+++ views/redemption.php
@@ -1,22 +1,21 @@
 <div class="section-page page-redemption">
 	<div class="close-section"><a href="#"><img src="img/page-closex.png"/></a></div>
 	<div class="page-head"><img src="img/redemption/label-redemption.png"/></div>
-	<div class="vid-box">
-		<div class="page-vidframe"></div>
-		<!--  -->
-		<div class="vid-copy">
-			<h2>LOREM IPSUM</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius hendrerit lacus, ac dapibus dia auctor a. Sed ornare augue purus, sit amet iaculis.</p>
+	<div class="page-row row-first">
+		<div class="vid-box">
+			<div class="page-vidframe"></div>
+			<!--  -->
+			<div class="vid-copy">
+				<h2>LOREM IPSUM</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius hendrerit lacus, ac dapibus dia auctor a. Sed ornare augue purus, sit amet iaculis.</p>
+			</div>
+		</div>
+		<div class="headquote">
+			<img src="img/redemption/redemption-headquote.png"/>
 		</div>
 	</div>
-	<div class="headquote">
-		<img src="img/redemption/redemption-headquote.png"/>
-	</div>
-	<div class="upperbg">
-		<!-- <img src="img/survival/survival-runnerbg.png"/> -->
-	</div>
 	<div class="page-row">
-		<div class="thumb">
+		<div class="thumb thumb1">
 			<a href="img/redemption/redemption-lg1.jpg" data-lightbox="redemption-set" data-title="">
 				<div class="hover-fx"><div class="expand-cta">EXPAND</div></div>
 				<img src="img/redemption/redemption-tn1.jpg" alt=""/>
@@ -51,13 +50,13 @@
 		<div class="thumb quote2">
 			<img src="img/redemption/redemption-quote2.png" alt=""/>
 		</div>		
-		<div class="thumb">
+		<div class="thumb thumb5">
 			
 			<a class="uncover" href="img/redemption/redemption-lg5.jpg" data-lightbox="redemption-set" data-title=""></a>	
 			<div class="hover-fx"><div class="expand-cta">EXPAND</div></div>
 			<img src="img/redemption/redemption-tn5.jpg" alt=""/>		
 		</div>
-		<div class="thumb">
+		<div class="thumb thumb6">
 			
 			<a class="uncover" href="img/redemption/redemption-lg6.jpg" data-lightbox="redemption-set" data-title=""></a>
 			<div class="hover-fx"><div class="expand-cta">EXPAND</div></div>
